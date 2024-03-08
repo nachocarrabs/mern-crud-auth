40df77c29d71b88d21e30ffd0ee80c35208681eb
ignacio <ignaciocarrabs@gmail.com>
2024-03-07 21:17:41 -0300

    conexion a base de datos y esquema

---
+++ src/models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        require: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true,
+    }
+})
+
+export default mongoose.model('User', userSchema)
