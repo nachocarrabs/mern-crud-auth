8a82e7cee5834c32ed6b9598255a0cb52f8f2308
ignacio <ignaciocarrabs@gmail.com>
2024-04-25 18:46:17 -0300

    registro de usuarios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <h1> Home </h1> } />
-        <Route path="/login" element={ <h1> Login </h1> } />
-        <Route path="/register" element={ <h1> Register </h1> } />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
         <Route path="/tasks" element={ <h1> Tasks </h1> } />
         <Route path="/add-task" element={ <h1> Add tasks </h1> } />
         <Route path="/task/:id" element={ <h1> Update task </h1> } />
